71b7cf6aca6676e249c2cf66150c22bec62a253c
UsrnGamesInc <71029215+Pi-Guy53@users.noreply.github.com>
2023-04-15 17:01:09 -0400

    working resistor calculator

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Resistor
+The Resistor calculator (TRC) is a calculator used for determining the value, and operating parameters of a resistor.
+Selecting the colors of the various bands will give you the Value, Ohmns, and tolerence of the resistor.
+This is useful for when you have a resistor of unknown type/orign, and want to be able to use it.
--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 import { getResistorOhms } from './resistor.js';
+
+let answer = document.querySelector('.answer');
+let lookUp = document.querySelector('.ceramic');
+
+let band1 = document.querySelector('#color0');
+let band2 = document.querySelector('#color1');
+let multi = document.querySelector('#color2');
+let toler = document.querySelector('#color3');
+
+let color1 = 'red';
+let color2 = 'green';
+let multiplier = 'violet';
+let tolerance = 'gold';
+
+for (let i = 0; i < band1.childNodes.length; i++) {
+    band1.childNodes[i].addEventListener('mousedown', (e) => {
+        //console.log(e.target.classList[1]);
+
+        color1 = e.target.classList[1];
+        updateAnswer();
+    });
+}
+
+for (let i = 0; i < band2.childNodes.length; i++) {
+    band2.childNodes[i].addEventListener('mousedown', (e) => {
+        //console.log(e.target.classList[1]);
+
+        color2 = e.target.classList[1];
+        updateAnswer();
+    });
+}
+
+for (let i = 0; i < multi.childNodes.length; i++) {
+    multi.childNodes[i].addEventListener('mousedown', (e) => {
+        //console.log(e.target.classList[1]);
+
+        multiplier = e.target.classList[1];
+        updateAnswer();
+    });
+}
+
+for (let i = 0; i < toler.childNodes.length; i++) {
+    toler.childNodes[i].addEventListener('mousedown', (e) => {
+        //console.log(e.target.classList[1]);
+
+        lookUp.childNodes[7].classList.remove(tolerance);
+        tolerance = e.target.classList[1];
+        updateAnswer();
+    });
+}
+
+
+updateAnswer();
+
+function updateAnswer() {
+
+    //update shown values
+    let resistor = getResistorOhms({ color1: color1, color2: color2, multiplier: multiplier, tolerance: tolerance });
+    answer.innerHTML = resistor;
+
+    //Update colors
+    lookUp.childNodes[1].classList = color1;
+    lookUp.childNodes[3].classList = color2;
+    lookUp.childNodes[5].classList = multiplier;
+    lookUp.childNodes[7].classList.add(tolerance);
+}
